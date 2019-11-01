1d1368a3fd82ad73e0478f7dfc2d84468b767fb9
Liv Johnson <livjab@gmail.com>
2019-11-01 12:40:26 -0500

    passing all tests

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,17 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
+    # hash starting location is key, destination as value
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
 
-    return route
+    # start the route
+    route = []
+    route.append(hash_table_retrieve(hashtable, "NONE"))
+
+    # go through each ticket and find next matching key value pair
+    while len(route) < length:
+        for ticket in tickets:
+            route.append(hash_table_retrieve(hashtable, route[-1]))
+
+    return route[:length]
